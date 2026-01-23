017d02aa69cdae0e5b43c661751bafe95d54f98f
Mohammad Shehar Yaar Tausif <sheharyaar48@gmail.com>
2026-01-23 16:42:38 +0530

    add MSR vm checks
    Signed-off-by: Mohammad Shehar Yaar Tausif <sheharyaar48@gmail.com>

--- nanovisor.c
+++ nanovisor.c
@@ -2,32 +2,7 @@
  * nanovisor - A Minimal Type 2 Hypervisor
  * Copyright (c) 2026 Mohammad Shehar Yaar Tausif <sheharyaar48@gmail.com>
  */
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/init.h>
-#include <linux/types.h>
-#include <linux/printk.h>
-
-#define FEAT_IDENT_CPUID 0x80000001
-#define FEAT_SVM_FEAT_CPUID 0x8000000A
-#define FEAT_SVM_OFF 2
-#define FEAT_X2AVIC_EXT_OFF 6
-
-#define FEAT_NP 0			    // Nested Paging
-#define FEAT_SVML 2			    // SVM Lock
-#define FEAT_VMCB_CLEAN 5		    // VMCB Clean Bits
-#define FEAT_VMSAVE_VIRT 15		    // VMSAVE and VMLOAD virtualization
-#define FEAT_ROGPT 21			    // Read-only Guest Page Tables
-#define FEAT_VNMI 25			    // NMI Virtualization
-#define FEAT_NESTED_VIRT_VMCB_ADDR_CHECK 28 // Guest VMCB Address Check
-
-#define TEST_SVM_FEAT(val, feat) ((val >> feat) & 0b01)
-
-struct svm_features {
-	uint32_t nasid;
-	uint32_t feat_mask;
-	bool x2avic_ext;
-};
+#include "nanovisor.h"
 
 static void print_svm_feat(struct svm_features *feat) {
 	pr_debug("mask=%x", feat->feat_mask);
@@ -53,37 +28,94 @@ static void print_svm_feat(struct svm_features *feat) {
 #undef TEST_PRINT_FEAT
 }
 
+// check support for SVM and see if it can be enabled
 static bool svm_enabled(void) {
-	uint32_t status = 0;
-	__asm__("mov $0x80000001, %%eax\n"
-		"cpuid\n"
-		: "=c"(status)::"eax", "ebx", "edx");
-	pr_debug("status=%x", status);
-	if ((status >> FEAT_SVM_OFF) & 0b01)
+	// check EFER.SVME if already enabled
+	uint32_t efer = 0;
+	__asm__(
+		"rdmsr\n"
+		: "=a"(efer)
+		: "c"(MSR_EFER)
+		: "edx"
+	);
+
+	bool svme_status = (efer >> 12) & 0b01;
+	if (svme_status) {
+		pr_info("SVM is already enabled");
 		return true;
-	else
+	}
+
+
+	// The CPU does not feature SVM
+	uint32_t status = 0;
+	__asm__("cpuid\n"
+			: "=c"(status)
+			: "a"(CPUID_FEAT)
+			: "ebx", "edx");
+	
+			pr_debug("status=%x", status);
+	if (!((status >> FEAT_SVM_OFF) & 0b01)) {
+		pr_err("SVM not available on the system");
 		return false;
+	}
+	
+	// The CPU features, check if it's disabled
+	uint32_t vm_cr = 0;
+	bool svmdis = 1;
+	__asm__(
+		"rdmsr\n"
+		: "=a"(vm_cr)
+		: "c"(MSR_VM_CR)
+		: "edx"
+	);
+
+	pr_debug("vm_cr: %#x", vm_cr);
+	svmdis = (vm_cr >> 4) & 0b01;
+
+	struct svm_features svm_feat;
+	unsigned int feat_x2avic = 0;
+	__asm__(
+		"cpuid\n"
+		: "=b"(svm_feat.nasid), "=c"(feat_x2avic),
+		  "=d"(svm_feat.feat_mask)
+		: "a"(CPUID_FEAT_SVM)
+		:);
+	svm_feat.x2avic_ext = (feat_x2avic >> FEAT_X2AVIC_EXT_OFF) & 0b01;
+
+	// SVM is disabled, check if it can be unlocked
+	if (svmdis) {
+		if ((svm_feat.feat_mask >> FEAT_SVML) & 0b01) {
+			pr_info("SVM can be unlocked using SVM Key, consult TPM or platform firmware for the key.");
+			return false;
+		} else {
+			pr_info("SVM must be enabled from frimware / BIOS");
+			return false;
+		}
+	}
+
+	// Try to enable SVM
+	// The CPU features, check if it's disabled
+	// efer |= (1 << 12);
+	// __asm__(
+	// 	"wrmsr\n"
+	// 	: 
+	// 	: "c"(MSR_VM_CR), "a"(efer)
+	// 	: "edx"
+	// );
+
+	print_svm_feat(&svm_feat);
+	return true;
 }
 
 static int __init nano_init(void)
 {
 	pr_info("Nanovisor Loaded\n");
 	if (!svm_enabled()) {
-		pr_err("SVM support not enabled");
+		// Operation not permitted ? Or other erro code
 		return 0;
 	}
 
 	pr_info("SVM support enabled");
-
-	struct svm_features svm_feat;
-	unsigned int feat_x2avic = 0;
-	__asm__("mov $0x8000000a, %%eax\n"
-		"cpuid\n"
-		: "=b"(svm_feat.nasid), "=c"(feat_x2avic),
-		  "=d"(svm_feat.feat_mask)::"eax");
-	svm_feat.x2avic_ext = (feat_x2avic >> FEAT_X2AVIC_EXT_OFF) & 0b01;
-
-	print_svm_feat(&svm_feat);
 
 	return 0;
 }
---
+++ nanovisor.h
@@ -0,0 +1,62 @@
+/*
+ * nanovisor - A Minimal Type 2 Hypervisor
+ * Copyright (c) 2026 Mohammad Shehar Yaar Tausif <sheharyaar48@gmail.com>
+ */
+#ifndef _NANOVISOR_H
+
+#include <linux/init.h>
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/printk.h>
+#include <linux/types.h>
+
+/* CPUID related defines for checking SVM support*/
+
+#define CPUID_FEAT 0x80000001
+#define CPUID_FEAT_SVM 0x8000000A
+#define FEAT_SVM_OFF 2
+#define FEAT_X2AVIC_EXT_OFF 6
+
+#define FEAT_NP 0                           // Nested Paging
+#define FEAT_SVML 2                         // SVM Lock
+#define FEAT_VMCB_CLEAN 5                   // VMCB Clean Bits
+#define FEAT_VMSAVE_VIRT 15                 // VMSAVE and VMLOAD virtualization
+#define FEAT_ROGPT 21                       // Read-only Guest Page Tables
+#define FEAT_VNMI 25                        // NMI Virtualization
+#define FEAT_NESTED_VIRT_VMCB_ADDR_CHECK 28 // Guest VMCB Address Check
+
+#define TEST_SVM_FEAT(val, feat) ((val >> feat) & 0b01)
+
+/* MSRs involved in SVM (from Appendix A, Setion A.7) */
+
+#define MSR_EFER 0xc0000080
+/* Guest View of Timestamp Counter, Section 15.30.5 */
+#define MSR_TSC_RATIO 0xc0010104
+/* Controls global aspect of SVM, Section 15.30.1 */
+#define MSR_VM_CR 0xc0010114
+/* IGNNE Signal, Section 15.30.2 */
+#define MSR_IGNNE 0xc0010115
+/* Control over SMM Signals, Section 15.30.3 */
+#define MSR_SMM_CTL 0xc0010116
+/* VM Host State Save Physical Address, Section 15.30.4 */
+#define MSR_VM_HSAVE_PA 0xc0010117
+#define MSR_SVM_KEY 0xc0010118
+#define MSR_DOORBELL 0xc001011B     /* Doorbell register */
+#define MSR_VMPAGE_FLUSH 0xc001011B /* SEV */
+#define MSR_GHCB 0xc0010130         /* GHCB */
+#define MSR_SEV_STATUS 0xc0010131
+#define MSR_RMP_BASE 0xc0010132
+#define MSR_RMP_END 0xc0010133
+#define MSR_GUEST_TSC_FREQ 0xc0010134    /* Secure TSC */
+#define MSR_VIRTUAL_TOM 0xc0010135       /* Virtual Top of Memory */
+#define MSR_SEGMENTED_RMP_CFG 0xc0010136 /* Segmented RMP */
+#define MSR_IDLE_WAKEUP_ICR 0xc0010137   /* Side Channel Protection */
+#define MSR_SECURE_AVIC_CTRL 0xc0010138  /* Secure AVIC */
+
+struct __attribute__((packed)) svm_features {
+  uint32_t nasid;
+  uint32_t feat_mask;
+  bool x2avic_ext;
+};
+
+#endif
